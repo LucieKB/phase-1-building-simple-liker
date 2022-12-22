213d47f311666c699ab269cb1ae9a2d678151d92
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2022-12-21 21:31:56 -0700

    commit tree 4c160d45271219b72a24c3b69651b9a74369a4d7

--- main.js
+++ main.js
@@ -4,7 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//Event Listeners
+const emojiHeart = (document.querySelectorAll(".like-glyph"))
+for (emoji of emojiHeart) {addEventListener('click', changeHeartColor)}
 
+//Event Handler
+
+function changeHeartColor(emojiHeart){
+  const heart = emojiHeart.target;
+  console.log(heart.innerText)
+  if (heart.innerText === EMPTY_HEART){
+    heart.innerText = FULL_HEART
+    heart.className = "activated-heart"
+  } else if (heart.innerText === FULL_HEART){
+    heart.innerText = EMPTY_HEART
+    heart.className = ""
+  }
+} 
+
+
+//Fetch request
+
+function likePost(){
+  mimicServerCall()
+  .then(heart => changeHeartColor(heart))
+  .catch(function(error){
+    alert (document.getElementsByClassName("hidden").remove())
+    setTimeOut(() => error.message, 3000)
+  })
+
+}
 
 
 //------------------------------------------------------------------------------
